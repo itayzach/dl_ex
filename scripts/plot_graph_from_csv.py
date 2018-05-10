aed4606472b2eeac4794f5e0e8d327adcedbdbad
Itay Zach <zitay.zach@gmail.com>
2018-05-10 18:30:19 +0300

    ex1 - plot_graph_from_csv

--- scripts/plot_graph_from_csv.py
+++ scripts/plot_graph_from_csv.py
@@ -1,25 +1,25 @@
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import csv
 import argparse
 import pandas as pd
 
+# Parse args
 parser = argparse.ArgumentParser(description='Plot graphs')
-parser.add_argument('--name', help='name of this run')
+parser.add_argument('--fname', help='csv filename path')
 args = parser.parse_args()
-if (not args.name):
-    print('gotta have a name')
-    exit()
 
-#headers = ['Epochs','Test accuracy (%)','Train accuracy (%)']
-df = pd.read_csv(args.name, delimiter='\t')
-print (df)
+# Get net name and optim
+path = args.fname.split('/')
+net, optim = path[-1].replace('.csv','').split('__')
 
-x = df['epoch']
-y1 = df['test_acc']
-y2 = df['train_acc']
+# Read csv
+df = pd.read_csv(args.fname, delimiter='\t')
+max_epoch = df['test_acc'].idxmax()
+test_acc_max = df['test_acc'].max()
 
-# plot
-plt.plot(x,y1,y2)
-plt.show()
+# Plot
+df.plot(title=net + ' with ' + optim + '\n' +
+        'max accuracy at epoch ' + str(max_epoch) + ' is ' + str(test_acc_max) + '%')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy [%]')
+plt.savefig('plots/' + net + '_' + optim + '.pdf')
